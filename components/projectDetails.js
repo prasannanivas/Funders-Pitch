c00b0af2ad95214eeb8ecdf4b90b7bf17d3a97de
prasannanivas <prasannanivasganguly@gmail.com>
2020-06-09 14:42:18 +0530

    kk"

--- components/projectDetails.js
+++ components/projectDetails.js
@@ -22,22 +22,22 @@ class Projectdetails extends Component {
 
   render() {
     return (
-      <div className="hover:bg-green-400 bg-gray-100">
+      <div className="hover:bg-purple-400 bg-gray-200">
         <a
           
           href={`/Campaigns/show/${this.props.address}`}
         >
           <div className="rounded overflow-hidden shadow-xl">
             <div className="p-2 mt-10">
-              <div className="font-bold text-xl mb-2">
+              <div className="text-4xl mb-2">
                 {this.state.projectTitle}
-              </div>
-              <p className="text-gray-700 text-base">
+              </div><hr className = "bg-pink-400 py-1"></hr>
+              <p className="text-gray-700 font-bold text-lg text-base">
                 {this.state.projectDescription}
               </p>
             </div>
             <div className="py-10">
-              <span className="inline-block bg-gray-200 rounded-full  text-lg p-5 font-semibold text-gray-700 mr-2">
+              <span className="inline-block bg-blue-400 rounded-full  text-lg p-5 font-semibold text-gray-700 mr-2">
                 #{this.state.projectPromise}
               </span>
             </div>
